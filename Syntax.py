28de28b4301e223ed650db20b75b26b3c07f6572
IanLi <jasmine_chieh_li@outlook.com>
2019-11-22 15:06:02 +0800

    a more complicated example tested

--- Syntax.py
+++ Syntax.py
@@ -27,11 +27,16 @@ class Syntactic:
             state = self.stack[-1][0]
             
             # validate token 
+            if self.head is None or self.head == ' ':
+                self.head = '$'
+
             if self.head not in self.t:
                 print('Invalid token of ' + self.head)
                 return False
 
             move = self.action[state][self.t[self.head]]
+            if move[0] == -1:
+                print('This is a breakpoint')
             status = self.make_action(move)
             if status == 0 or status == -1:
                 return
@@ -42,9 +47,10 @@ class Syntactic:
             # just to make the code more clear to understand           
             self.stack.append([move[1], deepcopy(self.head)])       # push into stack
             self.head = self.tokens.read(1)                         # shift right
-            if self.head is None or self.head == '':
+            if self.head is None or self.head == ' ':
                 self.head = '$'
-            
+
+            print('%-120s %s %d' % (self.stack, actions[move[0]], move[1]))
 
         elif actions[move[0]] == 'Reduce':
             production = self.productions[move[1]]
@@ -60,8 +66,10 @@ class Syntactic:
             self.stack.append([new_state, incoming])
             # push the new state and V into stack 
 
+            print('%-120s%s %s->%s' % (self.stack, actions[move[0]], production[0], production[1]))
+
         elif actions[move[0]] == 'ACCEPT':
-            print('Tokens accepted')
+            print('%-120s %s' % (self.stack, actions[move[0]]))
             return 0
     
         elif actions[move[0]] == 'ERROR':
@@ -87,24 +95,24 @@ if __name__ == '__main__':
                    ['F', '(E)'],
                    ['F', 'n']]
                 # E, T, F
-    go_table = [[1, 2, 3],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [10, 11, 12],
-                [-1, -1, -1],
-                [-1, 13, 3],
-                [-1, 14, 4],
-                [-1, -1, 15],
-                [-1, -1, 16],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1],
-                [-1, -1, -1]]
+    go_table = [[1, 2, 3],             # 0
+                [-1, -1, -1],          # 1
+                [-1, -1, -1],          # 2
+                [-1, -1, -1],          # 3
+                [10, 11, 12],          # 4
+                [-1, -1, -1],          # 5
+                [-1, 13, 3],           # 6
+                [-1, 14, 3],           # 7
+                [-1, -1, 15],          # 8
+                [-1, -1, 16],          # 9
+                [-1, -1, -1],          # 10 
+                [-1, -1, -1],          # 11
+                [-1, -1, -1],          # 12
+                [-1, -1, -1],          # 13
+                [-1, -1, -1],          # 14
+                [-1, -1, -1],          # 15
+                [-1, -1, -1],          # 16
+                [-1, -1, -1]]          # 17
 
     # actions = {1: 'Shift', 2: 'Reduce', 3: 'ACCEPT'}
     # {'n': 0, '+': 1, '-': 2, '*': 3, '/': 4, '(': 5, ')': 6, '$': 7}
@@ -118,14 +126,14 @@ if __name__ == '__main__':
                     [[1, 5], [-1], [-1], [-1], [-1], [1, 4], [-1], [-1]],  # 7
                     [[1, 5], [-1], [-1], [-1], [-1], [1, 4], [-1], [-1]],  # 8
                     [[1, 5], [-1], [-1], [-1], [-1], [1, 4], [-1], [-1]],  # 9
-                    [[-1], [1, 6], [1, 7], [-1], [-1], [-1], [1, 17], [-1], [-1]],  # 10
-                    [[-1], [2, 3], [2, 3], [1, 8], [1, 9], [-1], [-1], [2, 3], [2, 3]],  # 11
-                    [[-1], [2, 6], [2, 6], [2, 6], [2, 6], [-1], [-1], [2, 6], [2, 6]],  # 12
-                    [[-1], [2, 1], [2, 1], [1, 8], [1, 9], [-1], [-1], [2, 1], [2, 1]],  # 13
-                    [[-1], [2, 2], [2, 2], [1, 8], [1, 9], [-1], [-1], [2, 2], [2, 2]],  # 14
-                    [[-1], [2, 4], [2, 4], [2, 4], [2, 4], [-1], [-1], [2, 4], [2, 4]],  # 15
-                    [[-1], [2, 5], [2, 5], [2, 5], [2, 5], [-1], [-1], [2, 5], [2, 5]],  # 16
-                    [[-1], [2, 8], [2, 8], [2, 8], [2, 8], [-1], [-1], [2, 8], [2, 8]]]  # 17
+                    [[-1], [1, 6], [1, 7], [-1], [-1], [-1], [1, 17], [-1]],  # 10
+                    [[-1], [2, 3], [2, 3], [1, 8], [1, 9], [-1], [2, 3], [2, 3]],  # 11
+                    [[-1], [2, 6], [2, 6], [2, 6], [2, 6], [-1], [2, 6], [2, 6]],  # 12
+                    [[-1], [2, 1], [2, 1], [1, 8], [1, 9], [-1], [2, 1], [2, 1]],  # 13
+                    [[-1], [2, 2], [2, 2], [1, 8], [1, 9], [-1], [2, 2], [2, 2]],  # 14
+                    [[-1], [2, 4], [2, 4], [2, 4], [2, 4], [-1], [2, 4], [2, 4]],  # 15
+                    [[-1], [2, 5], [2, 5], [2, 5], [2, 5], [-1], [2, 5], [2, 5]],  # 16
+                    [[-1], [2, 7], [2, 7], [2, 7], [2, 7], [-1], [2, 7], [2, 7]]]  # 17
 
     analyzer = Syntactic(action_table, go_table, productions, example_v, example_t)
     analyzer.analyze('./Syntactic Analyzer Test File.txt')
